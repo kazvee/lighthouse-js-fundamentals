7b5bc85254d905202991282d6028825603525d73
karvok <kayvee123@gmail.com>
2023-03-19 22:45:05 +0000

    Add tinkering file from previous exercise

---
+++ tinkering.js
@@ -0,0 +1,31 @@
+// function multiply(a, b) {
+//   return a * b;
+// }
+// const result = multiply(2, 4);
+// console.log(result);
+
+
+
+// var celsius = 12;
+// var fahrenheit = celsius * 1.8 + 32; /* convert celsius to fahrenheit here */
+// console.log(fahrenheit);
+
+
+
+// if (musicians <= 0 ) 
+// {console.log("not a group") }
+// else if (musicians === 1) {
+//     console.log("solo");
+// } else if (musicians === 2) {
+//     console.log("duet") ;
+// } else if (musicians === 3) {
+//     console.log("trio");
+// } else if (musicians === 4) {
+//     console.log('quartet');
+// }
+// else (musicians > 4) {
+//     console.log("this is a large group");
+// };
+
+
+
