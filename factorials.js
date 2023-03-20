e0d335afed6694760901bb7c543c752578f76060
karvok <kayvee123@gmail.com>
2023-03-20 02:41:31 +0000

    Add files from previous exercises

---
+++ factorials.js
@@ -0,0 +1,6 @@
+let solution = 12
+
+for (let i = 1; i < 12; i++) {
+  solution *= i;
+}
+console.log(solution);
--- tinkering.js
+++ tinkering.js
@@ -5,27 +5,27 @@
 // console.log(result);
 
 
+// ++++++++++
+
 
 // var celsius = 12;
 // var fahrenheit = celsius * 1.8 + 32; /* convert celsius to fahrenheit here */
 // console.log(fahrenheit);
 
+// ++++++++++
 
 
-// if (musicians <= 0 ) 
-// {console.log("not a group") }
-// else if (musicians === 1) {
-//     console.log("solo");
-// } else if (musicians === 2) {
-//     console.log("duet") ;
-// } else if (musicians === 3) {
-//     console.log("trio");
-// } else if (musicians === 4) {
-//     console.log('quartet');
-// }
-// else (musicians > 4) {
-//     console.log("this is a large group");
-// };
-
-
-
+if (musicians <= 0 ) 
+{console.log("not a group") }
+else if (musicians === 1) {
+    console.log("solo");
+} else if (musicians === 2) {
+    console.log("duet") ;
+} else if (musicians === 3) {
+    console.log("trio");
+} else if (musicians === 4) {
+    console.log('quartet');
+}
+else (musicians > 4) {
+    console.log("this is a large group");
+};
